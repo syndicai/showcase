9813e3be15ea2298a18694e93168a4604dbde442
mlaskowski17 <marcin.ladesign@gmail.com>
2021-11-03 17:58:24 +0100

    fix main text and variables

--- src/components/heading.js
+++ src/components/heading.js
@@ -13,10 +13,9 @@ export default function Title() {
         <h2 className="mx-auto text-2xl md:text-3xl font-bold gradient-text tracking-tighter" style={{width: "min-content"}}>Showcase</h2>
         <h1 className="px-5 sm:px-auto mb-5 md:text-6xl tracking-tighter" >Let us inspire you</h1> 
         <Text type="secondary" className="px-5 sm:px-auto max-w-screen-md mx-auto">
-            Syndicai is on the mission to change to way you deliver and access AI. We are obssesed with 
-            the simplicity and passionate about AI. Our goal is to simplify deployment of any AI models
-            so that you will be able to ship real business value with your product fast.
-            Hope those sample demos will inspire you to create a next game-changer product powered by AI!
+          We do know that deploying machine learning models at scale is a very time-consuming
+          and complex process. Therefore we decided to build a platform that will allow 
+          you easily showcase your models. 
         </Text>
         <Text small p><a href={link.showcase_github}>Visit source code on GitHub.</a></Text>
       </div>
